881a04a1bfe72fd2a212b3aab4374e711ebec441
srinivasarao kondapaturi <srinivasisid@gmail.com>
2020-04-18 12:46:42 +0530

    added

---
+++ CoreReceiverApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace CoreReceiverApp
+{
+    class Program
+    {
+        /* 
+         * Go to your azure service bus resource , create if does not exists
+         * Go To Shared access policies 
+         * Go To RootManageSharedAccessKey 
+         * Copy Primary/Secondary  Connection String
+         */
+        const string ServiceBusConnectionString = "<your_connection_string>";
+        /*
+          * Go to your azure service bus resource , create if does not exists
+          * Create Queue , create if does not exists and use the same queue
+             */
+        const string QueueName = "<your_queue_name>";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+            RegisterOnMessageHandlerAndReceiveMessages();
+            Console.ReadKey();
+            await queueClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message : SequenceNumber:{message.SystemProperties.SequenceNumber} Body :{Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CoreSenderApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace CoreSenderApp
+{
+    class Program
+    {
+        /* 
+         * Go to your azure service bus resource , create if does not exists
+         * Go To Shared access policies 
+         * Go To RootManageSharedAccessKey 
+         * Copy Primary/Secondary  Connection String
+         */
+        const string ServiceBusConnectionString = "<your_connection_string>";
+        /*
+          * Go to your azure service bus resource , create if does not exists
+          * Create Queue , create if does not exists and use the same queue
+             */
+        const string QueueName = "<your_queue_name>";
+        static IQueueClient queueClient;
+
+        public static async Task Main(string[] args)
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            await SendMessageAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+
+        }
+
+        static async Task SendMessageAsync(int numberOfMessagesToSend)
+        {
+            try 
+            { 
+                for(var i = 0; i< numberOfMessagesToSend; i++)
+                {
+                    string messageBody = $"Message{i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    await queueClient.SendAsync(message);
+                }
+
+            }
+            catch (Exception exception) 
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }   
+        }
+    }
+}
